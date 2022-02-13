6d65f3eb25dcfc71233367def6c177766f519cc2
Slaven B <slaven.bunijevac@gmail.com>
2022-02-13 19:33:50 +0100

    init commit

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,11 @@
+import classes from './Card.module.css';
+
+const Card = () => {
+  return (
+    <div className={classes.card}>
+      <h1>Slaven Bunijevac</h1>
+    </div>
+  )
+}
+
+export default Card
